03d5fdf95a5b8d9b9a80f03e348b27f7c6a9104d
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-09-01 10:10:31 +0100

    Adding - Testimonials Section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,53 @@
 import "./testimonials.scss";
 
 export default function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: "Tom Durden",
+      title: "Senior Developer",
+      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/twitter.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+    },
+    {
+      id: 2,
+      name: "Alex Kalinski",
+      title: "Co-Founder of DELKA",
+      img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/youtube.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Martin Harold",
+      title: "CEO of ALBI",
+      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/linkedin.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
+
   return (
     <div id="testimonials" className="testimonials">
-      Testimonials
+      <h1>Testimonials</h1>
+      <div className="container">
+        {data.map(item => (
+          <div className={item.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img className="left" src="assets/right-arrow.png" alt="" />
+              <img className="user" src={item.img} alt="" />
+              <img className="right" src={item.icon} alt="" />
+            </div>
+            <div className="center">{item.desc}</div>
+            <div className="bottom">
+              <h3>Alex</h3>
+              <h4>{item.title}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
