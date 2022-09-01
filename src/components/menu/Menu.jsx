440de6dcb512c20ace87aee9fe88157bbb3f3825
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-09-01 17:04:37 +0100

    Updating - Styling

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -14,12 +14,6 @@ export default function menu({ menuOpen, setMenuOpen }) {
           <a href="#portfolio">Portfolio</a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
-          <a href="#works">Works</a>
-        </li>
-        <li onClick={() => setMenuOpen(false)}>
-          <a href="#testimonials">Testimonials</a>
-        </li>
-        <li onClick={() => setMenuOpen(false)}>
           <a href="#contact">Contact</a>
         </li>
       </ul>
