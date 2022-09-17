7dcca2d4760b16da081931964ba978bf6e52e038
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-09-17 15:05:46 +0100

    Adding - Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Projects from "./pages/projects/Projects";
 import Contact from "./pages/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./footer.scss";
+import PText from "../pText/PText";
+import FooterContent from "../footerContent/FooterContent";
+
+export default function Footer() {
+  return (
+    <footer className="footer-section">
+      <div className="footer-wrapper">
+        <div className="left">
+          <h2>Art&#363;ras Ju&#353;kys</h2>
+          <PText>
+            Aspiring web developer from Peterborough, UK. I love developing
+            responsive and fast web apps for today's world of mobile devices.
+          </PText>
+        </div>
+        <div className="right">
+          <FooterContent
+            heading="Important Links"
+            links={[
+              {
+                title: "Home",
+                url: "/",
+                type: "Link",
+              },
+              {
+                title: "About",
+                url: "/about",
+                type: "Link",
+              },
+              {
+                title: "Projects",
+                url: "/projects",
+                type: "Link",
+              },
+              {
+                title: "Contact",
+                url: "/contact",
+                type: "Link",
+              },
+            ]}
+          />
+          <FooterContent
+            heading="Social Links"
+            links={[
+              {
+                title: "LinkedIn",
+                url: "https://www.linkedin.com/in/arturas-juskys",
+              },
+              {
+                title: "GitHub",
+                url: "https://github.com/arturasjuskys",
+              },
+              {
+                title: "Facebook",
+                url: "https://www.facebook.com/arturas.ju/",
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <div className="copyright">
+        <PText>
+          &#169; 2022 - Art&#363;ras Ju&#353;kys | Design inspired by Web Cifar
+        </PText>
+      </div>
+    </footer>
+  );
+}
